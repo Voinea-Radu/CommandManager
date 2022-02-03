bc46b66fdbfc279f5fb4cd544b62a73a38426ba2
L1ghtDream <radu.voinea.mihai@gmail.com>
2022-02-03 19:33:18 +0200

    Added debugger messages

--- src/main/java/dev/lightdream/fly/manager/CommandManager.java
+++ src/main/java/dev/lightdream/fly/manager/CommandManager.java
@@ -2,9 +2,11 @@ package dev.lightdream.fly.manager;
 
 import dev.lightdream.fly.CommandMain;
 import dev.lightdream.fly.annotations.Command;
+import dev.lightdream.logger.Debugger;
 import dev.lightdream.logger.Logger;
 import org.reflections.Reflections;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +24,18 @@ public class CommandManager {
                     }
                 }
                 Object obj;
+                Debugger.info(aClass.getSimpleName() + " constructors: ");
+                for (Constructor<?> constructor : aClass.getDeclaredConstructors()) {
+                    StringBuilder parameters = new StringBuilder();
+                    for (Class<?> parameter : constructor.getParameterTypes()) {
+                        parameters.append(parameter.getSimpleName()).append(" ");
+                    }
+                    if (parameters.toString().equals("")) {
+                        Debugger.info("    - zero argument");
+                    } else {
+                        Debugger.info("    - " + parameters);
+                    }
+                }
                 if (aClass.getDeclaredConstructors()[0].getParameterCount() == 0) {
                     obj = aClass.getDeclaredConstructors()[0].newInstance();
                 } else if (aClass.getDeclaredConstructors()[0].getParameterCount() == 1) {
