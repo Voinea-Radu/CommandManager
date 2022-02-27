cf9449c37f487ae4a7c3d2d16d014bf6555ea1ee
LightDream <radu.voinea.mihai@gmail.com>
2022-02-27 10:04:44 +0200

    1.12

--- src/main/java/dev/lightdream/commandmanager/CommandMain.java
+++ src/main/java/dev/lightdream/commandmanager/CommandMain.java
@@ -14,6 +14,6 @@ public interface CommandMain {
     String getProjectVersion();
 
     @SuppressWarnings("unused")
-    CommandManager registerCommandManager();
+    CommandManager getCommandManager();
 
 }
