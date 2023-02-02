6efeceed4276024bb675834c50c3a6e3d1b53c76
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-02-03 01:15:36 +0200

    Version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,11 +16,11 @@ extra["luckperms"] = "5.4"
 extra["jetbrains-annotations"] = "23.1.0"
 
 // Project version
-extra["Forge_1_19_3"] = "2.1.8"
-extra["Spigot"] = "3.1.9"
-extra["Sponge"] = "2.1.8"
-extra["Velocity"] = "2.1.9"
-extra["Common"] = "1.1.14"
+extra["Forge_1_19_3"] = "2.1.9"
+extra["Spigot"] = "3.1.10"
+extra["Sponge"] = "2.1.9"
+extra["Velocity"] = "2.1.10"
+extra["Common"] = "1.1.15"
 
 // LightDream Libs
 extra["logger"] = "3.1.0"
