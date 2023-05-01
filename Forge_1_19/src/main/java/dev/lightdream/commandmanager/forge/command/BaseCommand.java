7e79a1d0d8a9ad23f746f8461d07ae00cf2ba9a3
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-05-01 22:33:21 +0300

    Improved impl of arguments in forge framework

--- Forge_1_19/src/main/java/dev/lightdream/commandmanager/forge/command/BaseCommand.java
+++ Forge_1_19/src/main/java/dev/lightdream/commandmanager/forge/command/BaseCommand.java
@@ -74,6 +74,7 @@ public abstract class BaseCommand implements CommonCommand {
         if (arguments.size() != 0) {
             if (arguments.size() != 1) {
                 for (int index = arguments.size() - 2; index >= 0; index--) {
+                    Debugger.log("Adding argument " + index+1 + " to " + index + " command");
                     RequiredArgumentBuilder<CommandSourceStack, ?> preLastArgument = arguments.get(index);
                     RequiredArgumentBuilder<CommandSourceStack, ?> lastArgument = arguments.get(index + 1);
 
